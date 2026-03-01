5a17cb0e79e0428785e75b94a4faf2158bd34295
Prism Developer <dev@prism.local>
2026-03-01 17:12:22 +0530

    Update Instagram link to CodeV GEHU official page

--- src/config.js
+++ src/config.js
@@ -14,7 +14,7 @@ export const EVENT_CONFIG = {
   totalSlots:       250,
   finaleTeams:      110,
   registrationUrl: "https://forms.google.com/YOUR_FORM_ID_HERE",
-  instagram:       "https://instagram.com/YOUR_HANDLE",
+  instagram:       "https://www.instagram.com/codev.gehu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
   linkedin:        "https://linkedin.com/company/YOUR_PAGE",
 };
 
